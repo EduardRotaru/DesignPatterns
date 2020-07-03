e3dd14e0f0ea3b3bfbc3e2f9abb700c20903bdd2
Eduard Ioan ROTARU <35904313+EduardRotaru@users.noreply.github.com>
2020-07-03 09:35:44 +0300

    ADAPTER

---
+++ VECTOR/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using static System.Console;
+using MoreLinq;
+using System.Collections;
+using System.Linq;
+
+namespace VECTOR
+{
+    public class Point
+    {
+        public int X, Y;
+
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        protected bool Equals(Point other)
+        {
+            return X == other.X && Y == other.Y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+
+            return Equals((Point) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (X * 397) ^ Y;
+            }
+        }
+    }
+
+    public class Line
+    {
+        public Point Start, End;
+
+        public Line(Point start, Point end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        protected bool Equals(Line other)
+        {
+            return Equals(Start, other.Start) && Equals(End, other.End);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Line)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((Start != null ? Start.GetHashCode() : 0) * 397) ^ (End != null ? End.GetHashCode() : 0);
+            }
+        }
+    }
+
+    public class VectorObject : Collection<Line>
+    {
+
+    }
+
+    public class VectorRectangle : VectorObject
+    {
+        public VectorRectangle(int x, int y, int width, int height)
+        {
+            Add(new Line(new Point(x, y), new Point(x + width, y)));
+            Add(new Line(new Point(x + width, y), new Point(x + width, y + height)));
+            Add(new Line(new Point(x, y), new Point(x, y + height)));
+            Add(new Line(new Point(x, y + height), new Point(x + width, y + height)));
+        }
+    }
+
+    public class LineToPointAdapter : IEnumerable<Point>
+        //: Collection<Point>
+    {
+        private static int count;
+        static Dictionary<int, List<Point>> cache =
+            new Dictionary<int, List<Point>>();
+        // Whats gonna be the key for the points its gonna be a hash code for the line
+        // we want to have a way of uniquely idenfity a line and calculating its hashcode
+        // because a line is composed of points and points are made of x,y coordinates which are unique 
+        // and its unlikely to get duplicates
+
+        public LineToPointAdapter(Line line)
+        {
+            // We are checking we already generated the values 
+            var hash = line.GetHashCode();
+            if (cache.ContainsKey(hash)) return;
+
+            WriteLine($"{++count}: Generating points for line [{line.Start.X},{line.Start.Y}]-[{line.End.X},{line.End.Y}]");
+
+            var points = new List<Point>();
+
+            int left = Math.Min(line.Start.X, line.End.X);
+            int right = Math.Max(line.Start.X, line.End.X);
+            int top = Math.Min(line.Start.Y, line.End.Y);
+            int bottom = Math.Max(line.Start.Y, line.End.Y);
+            int dx = right - left;
+            int dy = line.End.Y - line.Start.Y;
+
+            if (dx == 0)
+            {
+                for (int y = top; y <= bottom; ++y)
+                {
+                    points.Add(new Point(left, y));
+                }
+            }
+            else if (dy == 0)
+            {
+                for (int x = left; x <= right; ++x)
+                {
+                    points.Add(new Point(x, top));
+                }
+            }
+
+            cache.Add(hash, points);
+        }
+
+        public IEnumerator<Point> GetEnumerator()
+        {
+            return cache.Values.SelectMany(x => x).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    class Program
+    {
+        private static readonly List<VectorObject> vectorObjects
+           = new List<VectorObject>
+           {
+                new VectorRectangle(1,1,10,10),
+                new VectorRectangle(3,3,6,6)
+           };
+
+        // We get a set of coordinates but we can only draw by pixels.
+        // How we can adapt one to another
+        public static void DrawPoint(Point p)
+        {
+            Write(".");
+        }
+
+        static void Main(string[] args)
+        {
+            // It generates a lot of temporary information
+            // In our case invoking more than once means we generate points again and again
+            Draw();
+            Draw();
+
+            // need to use chaching
+        }
+
+        private static void Draw()
+        {
+            foreach (var vo in vectorObjects)
+            {
+                foreach (var line in vo)
+                {
+                    var adapter = new LineToPointAdapter(line);
+                    adapter.ForEach(DrawPoint);
+                }
+            }
+        }
+    }
+
+    // Conforming an interface we are given to the one we have 
+}
